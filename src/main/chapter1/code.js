f64da3281ae3edb2d3e37b77fd1d18b5ebb402ab
gorshkovayulia <gorshkovayulia93@gmail.com>
2023-01-13 15:38:27 +0200

    Create a separate .js file

--- src/main/chapter1/code.js
+++ src/main/chapter1/code.js
@@ -1,11 +1,15 @@
-scoops = 5;
-while (scoops > 0) {
-	document.write("Another scoop!<br>");
-	if (scoops < 3) {
-		alert("Ice cream is running low!");
-	} else if (scoops >= 5) {
-		alert("Eat faster, the ice cream is going to melt!");
-	}
-	scoops = scoops - 1;
+var word = "bottles";
+var count = 99;
+
+while (count > 0) {
+    console.log(count + " " + word + " of beer on the wall");
+    console.log(count + " " + word + " of beer,");
+    console.log("Take one down, pass it around,");
+    count = count - 1;
+
+    if (count > 0) {
+        console.log(count + " " + word + " of beer on the wall.");
+    } else {
+        console.log("No more " + word + " of beer on the wall.");
+    }
 }
-document.write("Life without ice cream isn't the same");
